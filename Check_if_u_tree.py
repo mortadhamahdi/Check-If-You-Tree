0ae88e4aa954d1236901f5580145e731e546416a
Mortadha Mahdi <46470418+mortadhamahdi@users.noreply.github.com>
2021-04-24 09:29:28 +0000

    Add files via upload

---
+++ Check_if_u_tree.py
@@ -0,0 +1,10 @@
+
+def main():
+
+    ilf=input("Type Your Country : ")
+    if ((ilf)== ("iraq") or (ilf)== ("IRAQ") or (ilf)== ("العراق") or (ilf)== ("Iraq")):
+        print("ها هلو شجرة")
+    else:
+        print("لووووز ورب الرادحات :)")
+
+if __name__ == '__main__':main()
